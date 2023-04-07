e77485f55073a9972d2dbdebc54868c739f62d30
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2023-04-07 22:55:37 +0600

    chore: form module test case added

--- src/form.rs
+++ src/form.rs
@@ -14,3 +14,22 @@ impl Form {
         self
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn form_new() {
+        let form: Form = Form::new();
+
+        assert_eq!(form.data, Vec::new());
+    }
+
+    #[test]
+    fn form_add() {
+        let form: Form = Form::new().add("key", "value");
+
+        assert_eq!(form.data, vec![("key", "value")]);
+    }
+}
