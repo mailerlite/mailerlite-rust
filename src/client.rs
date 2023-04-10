76718b43eb4664a18c655156470578aa24576957
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2023-04-10 11:29:39 +0600

    chore: client module test case added

--- src/client.rs
+++ src/client.rs
@@ -26,3 +26,37 @@ impl Client {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito::{Mock, Server, ServerGuard};
+
+    #[tokio::test]
+    async fn client_new() {
+        let mut server: ServerGuard = Server::new_async().await;
+        let mock: Mock = server
+            .mock("GET", "/hello")
+            .with_status(200)
+            .match_header("Accept", "application/json")
+            .match_header("Content-Type", "application/json")
+            .match_header("Authorization", "Bearer api_key")
+            .with_body(r#"{"message": "Hello world"}"#)
+            .create();
+
+        let client: Client = Client::new("api_key".to_string());
+
+        assert_eq!(
+            client
+                .request
+                .get(format!("{}/hello", server.url()))
+                .send()
+                .await
+                .expect("Failed to send request")
+                .status(),
+            200,
+        );
+        mock.assert();
+        assert!(mock.matched());
+    }
+}
